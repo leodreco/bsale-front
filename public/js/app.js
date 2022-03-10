1a6e2e79925985c7fb5c682cf62eb91c96704854
Alegre <leonardo.alegre.c@gmail.com>
2022-03-10 09:18:12 -0500

    Listado de productos

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,37 @@
 const searchBar = document.querySelector('#searchBar');
 const categoryList = document.querySelector('#categoryList');
+const productList = document.querySelector('#productList');
 
 (async () => {
-    let categoryResponse = await ApiService.categories();
+    let [productResponse, categoryResponse] = await Promise.all([
+        ApiService.products(),
+        ApiService.categories(),
+    ]);
+
+    if(productResponse.success){
+        categoryList.innerHTML = '';
+        for(let product of productResponse.data){
+            console.log(product);
+            productList.innerHTML += `
+                <div class="col" productId="${product.id}">
+                    <div class="card shadow-sm">
+                        <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="${product.url_image}"/>
+                
+                        <div class="card-body">
+                            <h3>${product.name}</h3>
+                            <p class="card-text">S/${product.price}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Agregar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
+    }
+
     if(categoryResponse.success){
         categoryList.innerHTML = '';
         for(let category of categoryResponse.data){
