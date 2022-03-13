072b6aacc3a73be29a1126c15fca3d0993de6642
Alegre <leonardo.alegre.c@gmail.com>
2022-03-13 08:45:35 -0500

    UrlQuerySerializa, transforma objetos a string compatible con el query del url

---
+++ public/js/UrlQuerySerialize.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * @param {*} data 
+ * @param {*} name Usado internamente para aplicar los nombres anidados ejm: filters[name][matchMode]=contains
+ * @returns 
+ */
+function UrlQuerySerialize(data, name = undefined){
+    let query = [];
+
+    if(typeof data === 'object'){
+        for(let key in data){
+            let value = data[key];
+            if(name != undefined){
+                key = `${name}[${key}]`;
+            }
+    
+            if(typeof value === 'string' || typeof value === 'number'){
+                query.push(`${key}=${value}`);
+            }else if(typeof value === 'object'){
+                for(let valueKey in value){
+                    query.push(querySerialize(value[valueKey], `${key}[${valueKey}]`));
+                }
+            }
+            
+        }
+    }else{
+        query.push(`${name}=${data}`);
+    }
+
+    return query.join('&');
+}
